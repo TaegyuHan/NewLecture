a27fcfbac193af374de4d38644e6678fe4c60921
TaegyuHan <gksxorb147@gmail.com>
2024-09-27 14:58:29 +0900

    Add: prototype, vue.js 양쪽 바인딩 (v-model, v-bind, v-on), React.js 사용방법

---
+++ rland-full/rland-reactjs/calc.js
@@ -0,0 +1,40 @@
+class Calc extends React.Component {
+  render() {
+    return (
+      <div>
+        <section id="calc" class="n-frame:rounded-shadow w:2/3">
+          <h1>계산기</h1>
+          <form class="n-form">
+            <div>
+              <label>
+                <span>X:</span>
+                <input type="text" dir="rtl" />
+              </label>
+            </div>
+            <div>
+              <label>
+                <span>Y:</span>
+                <input type="text" dir="rtl" />
+              </label>
+            </div>
+            <div>
+              <input
+                class="n-btn n-btn-color:main"
+                type="submit"
+                value="계산하기"
+              />
+            </div>
+            <div>
+              <label>
+                <span>Result:</span>
+                <input type="text" readonly dir="rtl" />
+              </label>
+            </div>
+          </form>
+        </section>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Calc />, document.getElementById("root"));
